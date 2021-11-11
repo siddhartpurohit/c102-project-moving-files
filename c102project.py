e57a4e02557b3738ed803f116c1c4c2d52300432
siddhartpurohit <74408824+siddhartpurohit@users.noreply.github.com>
2021-11-11 15:59:54 +0530

    Add files via upload

---
+++ c102project.py
@@ -0,0 +1,13 @@
+import os
+import shutil
+
+source=input("Enter Source Folder Name ")
+destination=input("Enter The Destination Folder Name  ")
+
+source = source +"/"
+destination = destination +"/"
+list_of_files = os.listdir(source)
+
+for file in list_of_files:
+    shutil.copy(source+file,destination)
+    
